65c29c15fbfe9b89582e335b7403d9abde0ba15e
Victoria Ludwig <vfludwig@mail.lipscomb.edu>
2021-03-15 23:41:24 -0500

    Added footer
    changed some css formatting

--- index.php
+++ index.php
@@ -5,19 +5,21 @@
         <link rel="stylesheet" href="css/secondwave.css">
 
     </head>
-
+    <header>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </header>
     <body>
 
     <ul class="navigation">
-        <li class="nav-item"><a href="#">Home</a></li>
-        <li class="nav-item"><a href="#">About</a></li>
+        <li class="nav-item"><a href="index.php">Home</a></li>
+        <li class="nav-item"><a href="about.html">About</a></li>
         <li class="nav-item"><a href="#">Contact</a></li>
     </ul>
 
     <input type="checkbox" id="nav-trigger" class="nav-trigger" />
     <label for="nav-trigger"></label>
 
-<div class="site-wrap">
+    <div class="site-wrap">
 
         <div class="main">
             <br><br><br><br><br><br><br>
@@ -39,8 +41,14 @@
 
             </form>
 
-            <p>Developed by Asylsum Studios</p>
         </div>
+
+        <div class="footer"><br>
+            <p>Developed by Asylum Studios</p>
         </div>
+
+    </div>
+
+
     </body>
 </html>
