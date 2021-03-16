2ec194b60d00d2e60f8e10e0bee2c0617eed250e
Victoria Ludwig <vfludwig@mail.lipscomb.edu>
2021-03-15 22:16:38 -0500

    Basic Website based on Webflow website

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+
+    <head>
+
+        <link rel="stylesheet" href="secondwave.css">
+
+    </head>
+
+    <body>
+        <div class="main">
+            <img src="menufont.png" alt="The Second Wave">
+            
+            <br><br>
+
+            <h1>Developed by Asylum Studios</h1>
+
+            <br><br><br>
+
+            <h2>Sign up for Notifications</h2>
+
+
+            <br><br>
+            <form action="insert.php" method="post">
+
+            <input class="inputtext" type="text" name="email" placeholder="Your email address" /><br>
+
+            <input class="button" type="submit" />
+
+            </form>
+        </div>
+    </body>
+</html>
